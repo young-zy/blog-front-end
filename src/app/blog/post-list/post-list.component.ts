0aaffe42a3f3b54dfc29ea1e8b9463cd0a965167
young-zy <youngzhaozhou@gmail.com>
2021-11-13 17:14:25 +0800

    update package lock add some impl to post list

--- src/app/blog/post-list/post-list.component.ts
+++ src/app/blog/post-list/post-list.component.ts
@@ -1,16 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { PostApiService } from '../post-api.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Post } from '../../common/entities/post';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.scss']
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
 
-  constructor() {
+  public postList: Post[] = [];
+
+  public totalCount = 0;
+
+  public currentPage = 1;
+
+  public size = 10;
+
+  public loading = false;
+
+  private subscriptionArray: Subscription[] = [];
+
+  constructor(private postApiService: PostApiService,
+              private router: Router,
+              private route: ActivatedRoute) {
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptionArray.forEach(subscription => {
+      subscription.unsubscribe();
+    });
   }
 
   ngOnInit(): void {
+    const subscription = this.route.queryParams.subscribe(value => {
+      this.currentPage = value.page;
+      this.size = value.size;
+      this.loadPosts();
+    });
+    this.subscriptionArray.push(subscription);
   }
 
+  public loadPosts(): void {
+    this.postApiService.getPostList(this.currentPage, this.size).subscribe(resp => {
+      this.postList = resp.posts;
+      this.totalCount = resp.totalCount;
+    });
+  }
 }
