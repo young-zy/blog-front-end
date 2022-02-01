8529f0b2ce3be96c6ce7873918b8edc6b4ba56fe
young-zy <youngzhaozhou@gmail.com>
2022-02-01 19:34:30 +0800

    blog implementation preparation

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -3,13 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { BlogRoutingModule } from './blog-routing.module';
 import { PostListComponent } from './post-list/post-list.component';
+import { PostComponent } from './post/post.component';
+import { TocComponentComponent } from './toc-component/toc-component.component';
+import { MarkdownModule } from 'ngx-markdown';
+import { MatCardModule } from '@angular/material/card';
+import { MarkdownSummaryPipe } from './markdown-summary.pipe';
 
 
 @NgModule({
-  declarations: [PostListComponent],
+  declarations: [PostListComponent, PostComponent, TocComponentComponent, MarkdownSummaryPipe],
   imports: [
     CommonModule,
-    BlogRoutingModule
+    BlogRoutingModule,
+    MarkdownModule,
+    MatCardModule
   ]
 })
 export class BlogModule {
