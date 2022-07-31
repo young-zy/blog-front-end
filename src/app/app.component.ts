da256a46503a835ac1b1f86946138e39db612d9a
young-zy <youngzhaozhou@gmail.com>
2022-08-01 03:45:38 +0800

    add service worker update logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { ApplicationRef, Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { TitleService } from './common/title.service';
-import { filter, map, mergeMap } from 'rxjs/operators';
+import { filter, first, map, mergeMap } from 'rxjs/operators';
 import { NavigationComponent } from './navigation/navigation.component';
+import { SwUpdate } from '@angular/service-worker';
+import { concat, interval } from 'rxjs';
 
 @Component({
   standalone: true,
@@ -15,9 +17,34 @@ import { NavigationComponent } from './navigation/navigation.component';
 })
 export class AppComponent implements OnInit{
   title = 'blog';
+
   constructor(private router: Router,
               private activatedRoute: ActivatedRoute,
-              private titleService: TitleService) {
+              private titleService: TitleService,
+              private appRef: ApplicationRef,
+              private updates: SwUpdate) {
+    updates.versionUpdates.subscribe(event => {
+      switch (event.type) {
+        case 'VERSION_DETECTED':
+          console.log(`Downloading new app version: ${event.version.hash}`);
+          break;
+        case 'VERSION_READY':
+          console.log(`Current app version: ${event.currentVersion.hash}`);
+          console.log(`New app version ready for use: ${event.latestVersion.hash}`);
+          // TODO show notification
+          break;
+        case 'VERSION_INSTALLATION_FAILED':
+          console.log(`Failed to install app version '${event.version.hash}': ${event.error}`);
+          break;
+      }
+    });
+    // Allow the app to stabilize first, before starting
+    // polling for updates with `interval()`.
+    const appIsStable$ = appRef.isStable.pipe(first(isStable => isStable));
+    const everySixHours$ = interval(6 * 60 * 60 * 1000);
+    const everySixHoursOnceAppIsStable$ = concat(appIsStable$, everySixHours$);
+
+    everySixHoursOnceAppIsStable$.subscribe(() => updates.checkForUpdate());
   }
 
   ngOnInit(): void {
